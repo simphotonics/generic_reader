46c907155abcc4c0167d7505322424579993f63e
simphotonics <git@simphotonics.com>
2020-07-28 16:38:55 +0100

    Deleted.

--- example/src/wrapper_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:generic_reader/src/test_types/wrapper.dart';
-
-/// User to the [Wrapper].
-class WrapperTest {
-  const WrapperTest();
-  final wrappedVariable = const Wrapper<double>(27.9);
-}
