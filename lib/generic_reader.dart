636a8ae3945b93fe4a8e6201f25ede824ea71f7e
simphotonics <git@simphotonics.com>
2020-07-28 16:40:15 +0100

    Added export directives.

--- lib/generic_reader.dart
+++ lib/generic_reader.dart
@@ -1,6 +1,9 @@
-/// Library providing a customizable generic reader aimed at creating runtime objects from
-/// a static representation such as [ConstantReader] or [DartObject].
+/// Library providing a customizable generic reader aimed at creating
+/// runtime constant objects from a static representation
+/// of a compile-time constant expression such as [source_gen.ConstantReader]
+/// or [analyzer.DartObject].
 library generic_reader;
 
 export 'src/readers/generic_reader.dart';
-export 'src/errors/reader_error.dart';
+export 'src/extensions/type_methods.dart';
+export 'src/error_types/invalid_type_argument.dart';
