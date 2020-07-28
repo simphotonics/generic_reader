b0020eb8ab9426a25df91dca0a6033f5acada8f7
simphotonics <git@simphotonics.com>
2020-07-28 16:39:21 +0100

    Added example related to maps.

--- example/bin/player_example.dart
+++ example/bin/player_example.dart
@@ -1,10 +1,11 @@
 import 'package:ansicolor/ansicolor.dart';
+import 'package:exception_templates/exception_templates.dart';
 import 'package:generic_reader/generic_reader.dart';
 import 'package:source_gen/source_gen.dart' show ConstantReader;
+import 'package:source_gen_test/source_gen_test.dart';
 import 'package:source_gen_test/src/init_library_reader.dart';
-import 'package:generic_reader/src/test_types/column.dart';
-import 'package:generic_reader/src/test_types/sqlite_type.dart';
-import 'package:generic_reader/src/test_types/sponsor.dart';
+
+import 'package:generic_reader_example/generic_reader_example.dart';
 
 /// To run this program navigate to the folder: /example
 /// in your local copy the package [generic_reader] and
@@ -19,7 +20,7 @@ import 'package:generic_reader/src/test_types/sponsor.dart';
 Future<void> main() async {
   /// Reading libraries.
   final playerLib = await initializeLibraryReaderForDirectory(
-    'example/src',
+    'example/lib/src',
     'player.dart',
   );
 
@@ -37,29 +38,49 @@ Future<void> main() async {
   final sponsorsCR =
       ConstantReader(playerLib.classes.first.fields[3].computeConstantValue());
 
+  final greekCR =
+      ConstantReader(playerLib.classes.first.fields[6].computeConstantValue());
+
+  final mapCR =
+      ConstantReader(playerLib.classes.first.fields[7].computeConstantValue());
+
+  final mapWithEnumEntryCR =
+      ConstantReader(playerLib.classes.first.fields[8].computeConstantValue());
+
   // Get singleton instance of the reader.
   final reader = GenericReader();
 
-  // Decoders for [SqliteType] and its derived types.
-  final Decoder<Integer> integerDecoder =
-      (cr) => (cr == null) ? null : Integer(cr.peek('value')?.intValue);
+  Integer integerDecoder(ConstantReader cr) {
+    if (cr == null) return null;
+    return Integer(cr.peek('value')?.intValue);
+  }
 
-  final Decoder<Real> realDecoder =
-      (cr) => (cr == null) ? null : Real(cr.peek('value')?.doubleValue);
+  Real realDecoder(ConstantReader cr) {
+    if (cr == null) return null;
+    return Real(cr.peek('value')?.doubleValue);
+  }
 
-  final Decoder<Boolean> booleanDecoder =
-      (cr) => (cr == null) ? null : Boolean(cr.peek('value')?.boolValue);
+  Boolean booleanDecoder(ConstantReader cr) {
+    if (cr == null) return null;
+    return Boolean(cr.peek('value')?.boolValue);
+  }
 
-  final Decoder<Text> textDecoder =
-      (cr) => (cr == null) ? null : Text(cr.peek('value')?.stringValue);
+  Text textDecoder(ConstantReader cr) {
+    if (cr == null) return null;
+    return Text(cr.peek('value')?.stringValue);
+  }
 
-  final Decoder<SqliteType> sqliteTypeDecoder = ((cr) {
+  SqliteType sqliteTypeDecoder(ConstantReader cr) {
     if (cr == null) return null;
     if (reader.holdsA<Integer>(cr)) return reader.get<Integer>(cr);
     if (reader.holdsA<Text>(cr)) return reader.get<Text>(cr);
     if (reader.holdsA<Real>(cr)) return reader.get<Real>(cr);
-    return reader.get<Boolean>(cr);
-  });
+    if (reader.holdsA<Boolean>(cr)) return reader.get<Boolean>(cr);
+    throw ErrorOf<Decoder<SqliteType>>(
+        message: 'Could not reader const value of type `SqliteType`',
+        invalidState: 'ConstantReader holds a const value of type '
+            '`${cr.objectValue.type}`.');
+  }
 
   // Registering decoders.
   reader
@@ -85,16 +106,19 @@ Future<void> main() async {
       );
     }
 
-    if (reader.holdsA<Column>(cr, typeArgs: [Text]))
+    if (reader.holdsA<Column>(cr, typeArgs: [Text])) {
       return columnFactory<Text>();
-    if (reader.holdsA<Column>(cr, typeArgs: [Real]))
+    }
+    if (reader.holdsA<Column>(cr, typeArgs: [Real])) {
       return columnFactory<Real>();
-    if (reader.holdsA<Column>(cr, typeArgs: [Integer]))
+    }
+    if (reader.holdsA<Column>(cr, typeArgs: [Integer])) {
       return columnFactory<Integer>();
+    }
     return columnFactory<Boolean>();
   });
 
-  AnsiPen green = AnsiPen()..green(bold: true);
+  final green = AnsiPen()..green(bold: true);
 
   // Retrieve an instance of [String].
   final columnName = reader.get<String>(columnNameCR);
@@ -129,10 +153,36 @@ Future<void> main() async {
 
   final id = reader.get<Column>(idCR);
   print('');
-  print(green('Retrieving a [Column<Integer>]:'));
+  print(green('Retrieving a Column<Integer>:'));
   print(id);
   // Prints:
   // Retrieving a [Column<Integer>]:
   // Column<Integer>(
   // )
+
+  final greek = reader.getEnum<Greek>(greekCR);
+  print('');
+  print(green('Retrieving an instance of the '
+      'enumeration: Greek{alpha, beta}.'));
+  print(greek);
+  // Prints:
+  // 'Retrieving an instance of the enumeration: Greek{alpha, beta}.'
+  // Greek.alpha
+
+  final map = reader.getMap<String, dynamic>(mapCR);
+  print('');
+  print(green('Retrieving a Map<String, dynamic>:'));
+  print(map);
+  // Prints:
+  // 'Retrieving a Map<String, dynamic>:'
+  // {one: 1, two: 2.0}
+
+  reader.addDecoder<Greek>((cr) => cr.enumValue<Greek>());
+  final mapWithEnumEntry = reader.getMap<String, dynamic>(mapWithEnumEntryCR);
+  print('');
+  print(green('Retrieving a Map<String, dynamic>:'));
+  print(mapWithEnumEntry);
+  // Prints:
+  // 'Retrieving a Map<String, dynamic>:'
+  // {one: 1, two: 2.0, enum: Greek.alpha}
 }
