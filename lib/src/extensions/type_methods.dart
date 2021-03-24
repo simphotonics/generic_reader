d4b4afe1fbae3efb465d402fa240733ea047c6a0
simphotonics <git@simphotonics.com>
2021-03-24 13:54:39 +0000

    Added type methods.

--- lib/src/extensions/type_methods.dart
+++ lib/src/extensions/type_methods.dart
@@ -1,57 +1,43 @@
-import 'dart:mirrors';
-
 import 'package:analyzer/dart/element/type.dart';
-import 'package:exception_templates/exception_templates.dart';
-import 'package:generic_reader/src/error_types/invalid_type_argument.dart';
 import 'package:source_gen/source_gen.dart' show ConstantReader;
 
 /// Extension adding the method `isDynamic`
 extension TypeMethods on ConstantReader {
   /// Returns `true` if `this` represents a constant expression
   /// with type `dynamic`.
-  bool get isDynamic => objectValue.type.isDynamic;
+  bool get isDynamic => objectValue.type?.isDynamic ?? false;
+
+  /// Returns `true` is `this` represents a constant expression with
+  /// type exactly [Iterable].
+  ///
+  /// Note: Returns `false` if the static type represents [List] or [Set].
+  bool get isIterable => objectValue.type?.isDartCoreIterable ?? false;
+
+  /// Returns `true` if the static type represents
+  /// [List], [Set], [Map], or [Iterable].
+  bool get isCollection => isList || isSet || isMap || isIterable;
+
+  bool get isRecursiveCollection {
+    if (isNotCollection) return false;
+    final typeArg = dartTypeArgs[0];
+    if (typeArg.isDartCoreIterable ||
+        typeArg.isDartCoreList ||
+        typeArg.isDartCoreSet ||
+        typeArg.isDartCoreMap) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /// Returns `true` if the static type does not represent
+  /// [List], [Set], [Map], or [Iterable].
+  bool get isNotCollection => !isList && !isSet && !isMap && !isIterable;
 
   /// Returns the static type of `this`.
-  DartType get type => objectValue.type;
+  DartType? get dartType => objectValue.type;
 
   /// Returns a `List` of type arguments or the empty list.
-  List<DartType> get typeArgs => objectValue.type.typeArguments ?? <DartType>[];
-
-  /// Reads a instance of a Dart enumeration.
-  ///
-  /// Throws [ErrorOf] if a constant cannot be read.
-  T enumValue<T>() {
-    if (T == dynamic) {
-      throw ErrorOfType<InvalidTypeArgument>(
-          message: 'Method getEnum does not work with type: dynamic.',
-          expectedState: 'A type argument "T" in getEnum<T> '
-              'that represents a Dart enum.');
-    }
-
-    final classMirror = reflectClass(T);
-    final typeMirror = reflectType(T);
-    if (!classMirror.isEnum) {
-      throw ErrorOfType<InvalidTypeArgument>(
-          message: 'Could not read constant via enumValue<$T>().',
-          invalidState: '$T is not a Dart enum.');
-    }
-    final varMirrors = <VariableMirror>[];
-    for (final item in classMirror.declarations.values) {
-      if (item is VariableMirror && item.type == typeMirror) {
-        varMirrors.add(item);
-      }
-    }
-    // Access enum field 'values'.
-    final values = classMirror.getField(const Symbol('values')).reflectee;
-    for (final varMirror in varMirrors) {
-      final name = MirrorSystem.getName(varMirror.simpleName);
-      final index = peek(name)?.intValue;
-      if (index != null) {
-        return values[index];
-      }
-    }
-    throw ErrorOf<ConstantReader>(
-        message: 'Could not read enum '
-            'instance of type $T.');
-  }
+  List<DartType> get dartTypeArgs =>
+      objectValue.type?.typeArguments ?? <DartType>[];
 }
