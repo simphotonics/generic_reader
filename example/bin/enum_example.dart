ce505edcb2498b0481acf099e1cd2197622d7470
simphotonics <git@simphotonics.com>
2025-10-17 17:59:12 +0100

    Updated examples.

---
+++ example/bin/enum_example.dart
@@ -0,0 +1,42 @@
+import 'package:ansi_modifier/ansi_modifier.dart';
+import 'package:build_test/build_test.dart' show resolveSource;
+import 'package:generic_reader/generic_reader.dart';
+
+/// To run this program navigate to the root folder
+/// in your local copy the package `generic_reader` and
+/// use the command:
+///
+/// # dart example/bin/enum_example.dart
+
+/// Demonstrates how to use [Reader] to read an enum.
+enum Order { asc, desc }
+
+Future<void> main() async {
+  print('\nReading library: example\n');
+
+  final library = await resolveSource(
+    r'''
+    library example;
+
+    enum Order { asc, desc }
+
+    class A {
+      const A();
+      final Order order = Order.asc;
+    }
+    ''',
+    (resolver) => resolver.findLibraryByName('example'),
+    readAllSourcesFromFilesystem: false,
+  );
+
+  final lib = library!;
+  Reader.addDecoder(const EnumDecoder<Order>(Order.values));
+
+  final enumObj = lib.classes[0].fields[0].computeConstantValue();
+  final enum0 = enumObj?.read<Order>();
+
+  print(
+    '\n mroe Reading an enum of type ${'Order'.style(Ansi.green)}: '
+    '$enum0\n',
+  );
+}
---
+++ example/bin/list_example.dart
@@ -0,0 +1,49 @@
+import 'package:ansi_modifier/ansi_modifier.dart';
+import 'package:build_test/build_test.dart' show resolveSource;
+import 'package:generic_reader/generic_reader.dart';
+
+/// To run this program navigate to the root folder
+/// in your local copy the package `generic_reader` and
+/// use the command:
+///
+/// # dart example/bin/enum_example.dart
+
+/// Demonstrates how to use [Reader] to read a nested list.
+Future<void> main() async {
+  print('\nReading library: example\n');
+
+  final lib = await resolveSource(
+    r'''
+    library example;
+
+    class A {
+      const A();
+      final nestedList = List<List<String>> [['a'], ['b']];
+    }
+    ''',
+    (resolver) => resolver.findLibraryByName('example'),
+    readAllSourcesFromFilesystem: false,
+  );
+
+  if (lib == null) return;
+
+  final listOfString = 'List<String>'.style(Ansi.green);
+  final listOfListOfString = 'List<List<String>>'.style(Ansi.green);
+
+  print('\nAdding decoder for $listOfString and $listOfListOfString\n');
+  Reader.addDecoder(const ListDecoder<String>());
+  Reader.addDecoder(const ListDecoder<List<String>>());
+
+  print(Reader.info);
+
+  final listObj = lib.classes[0].fields[0].computeConstantValue();
+  final list1 = listObj?.read<List<List<String>>>();
+  final list2 = listObj?.read();
+  final list3 = listObj?.readList<List<String>>();
+
+  print('\nlistObj.read<$listOfListOfString>: $list1');
+
+  print('\nlistObj.read(): $list2');
+
+  print('\nlistObj.readList<$listOfString>(): $list3\n');
+}
