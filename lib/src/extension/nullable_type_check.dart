bae8023b2f8cdd328743465ff696aed66559a3b0
simphotonics <git@simphotonics.com>
2025-10-17 18:03:23 +0100

    Removed file.

--- lib/src/extension/nullable_type_check.dart
+++
@@ -1,7 +0,0 @@
-extension NullableTypeCheck on Object? {
-  /// Returns `true` if the object is null, and `false` otherwise.
-  bool get isNull => this == null ? true : false;
-
-  /// Returns `true` if the object is not null, and `false` otherwise.
-  bool get isNotNull => this == null ? false : true;
-}
